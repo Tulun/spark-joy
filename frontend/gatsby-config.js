0b24605626b4eb92a6bee0f7219d546a1ea66eb5
Jason Kiraly <jaykiraly@gmail.com>
2019-06-25 10:54:04 -0700

    Setup reakit and styled components.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Spark Joy`,
+    description: `Ask your fans if what you made sparked joy`,
+    author: `@tulun`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ frontend/src/components/theme.js
@@ -0,0 +1,4 @@
+export default {
+  // ...basics,
+  // Add component overrides here.
+}
