d7287c80bd4fece829329d8c79a6670310c5f402
Tulun <jaykiraly@gmail.com>
2019-10-30 16:50:47 -0700

    Stopping for part 2. Basic widget rendering.

--- frontend/src/components/WidgetBuilder.js
+++ frontend/src/components/WidgetBuilder.js
@@ -1,13 +1,31 @@
 import React from "react";
 import styled from "styled-components";
-import { Heading } from "./styles";
+import { Heading, Flex } from "./styles";
 import { 
   unstable_Form as Form, 
   unstable_FormInput as FormInput,
   unstable_useFormState as UseFormState
 } from "reakit/Form";
+import { palette } from "styled-tools";
 
-console.log(FormInput)
+
+const RoundButton = styled.button`
+  border-radius: 100%;
+  font-size: ${palette("headings", 0)};
+  line-height: ${palette("headings", 0)};
+  height: 2em;
+  width: 2em;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const Layout = styled.div`
+  width: 450px;
+`
+
 const WidgetBuilder = () => {
   const form  = UseFormState({
     values: {
@@ -15,11 +33,19 @@ const WidgetBuilder = () => {
     }
   })
   return (
-    <Form {...form}>
-      <Heading h1>
-        Did this {<FormInput name="type_of_joy" {...form} /> }spark joy?
-      </Heading>
-    </Form>
+    <Layout>
+      <Form {...form}>
+        <Heading h1>
+          Did this 
+          {<FormInput name="type_of_joy" {...form} /> }
+          spark joy?
+          <Flex row>
+            <RoundButton>👎</RoundButton>
+            <RoundButton>👍</RoundButton>
+          </Flex>
+        </Heading>
+      </Form>
+    </Layout>
   )
 }
 
