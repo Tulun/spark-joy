a9c50bb898746301a8267441964cebceceacf38e
Tulun <jaykiraly@gmail.com>
2019-10-30 16:17:37 -0700

    Did I fix it the way he did. LEts find out.

--- frontend/src/components/WidgetBuilder.js
+++ frontend/src/components/WidgetBuilder.js
@@ -1,14 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { Heading } from "./styles";
+import { 
+  unstable_Form as Form, 
+  unstable_FormInput as FormInput,
+  unstable_useFormState as UseFormState
+} from "reakit/Form";
 
+console.log(FormInput)
 const WidgetBuilder = () => {
+  const form  = UseFormState({
+    values: {
+      type_of_joy: ""
+    }
+  })
   return (
-    <>
+    <Form {...form}>
       <Heading h1>
-        Did this X spark joy?
+        Did this {<FormInput name="type_of_joy" {...form} /> }spark joy?
       </Heading>
-    </>
+    </Form>
   )
 }
 
